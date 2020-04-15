5e13375e77f0278c98a9cd31f125df772c4b0795
Günther Foidl <gue@korporal.at>
2020-04-15 22:12:50 +0200

    Updates

--- tests/mathTests/add.ts
+++ tests/mathTests/add.ts
@@ -1,25 +1,25 @@
 //import { Math } from "../../lib/math";
 import { Math } from "../../lib/index";
-import { TestFixture, TestCase, Test, Expect } from "alsatian";
+import { TestFixture, TestCase, Expect } from "alsatian";
 //-----------------------------------------------------------------------------
 @TestFixture()
 export class Math_Add {
-    @TestCase(0, 0, 0)
-    @TestCase(1, 2, 3)
-    @TestCase(2, 1, 3)
-    public Summands_given___correct_sum(a: number, b: number, res: number): void {
-        const sut = new Math();
+	@TestCase(0, 0, 0)
+	@TestCase(1, 2, 3)
+	@TestCase(2, 1, 3)
+	public Summands_given___correct_sum(a: number, b: number, res: number): void {
+		const sut = new Math();
 
-        const actual = sut.Add(a, b);
+		const actual = sut.Add(a, b);
 
-        Expect(actual).toBe(res);
-    }
-    //-------------------------------------------------------------------------
-    @TestCase(9007199254740992, 1)
-    @TestCase(1, 9007199254740992)
-    public MaxValue_plus_1___throws_RangeError(a: number, b: number): void {
-        const sut = new Math();
+		Expect(actual).toBe(res);
+	}
+	//-------------------------------------------------------------------------
+	@TestCase(9007199254740992, 1)
+	@TestCase(1, 9007199254740992)
+	public MaxValue_plus_1___throws_RangeError(a: number, b: number): void {
+		const sut = new Math();
 
-        Expect(() => sut.Add(a, b)).toThrowError(RangeError, "Summand(s) must not be MAX_SAFE_INTEGER (or greater)");
-    }
+		Expect(() => sut.Add(a, b)).toThrowError(RangeError, "Summand(s) must not be MAX_SAFE_INTEGER (or greater)");
+	}
 }
--- tests/mathTests/subtract.ts
+++ tests/mathTests/subtract.ts
@@ -1,17 +1,17 @@
 //import { Math } from "../../lib/math";
 import { Math } from "../../lib/index";
-import { TestFixture, TestCase, Test, Expect } from "alsatian";
+import { TestFixture, TestCase, Expect } from "alsatian";
 //-----------------------------------------------------------------------------
 @TestFixture()
 export class Math_Subtract {
-    @TestCase(0, 0, 0)
-    @TestCase(1, 2, -1)
-    @TestCase(2, 1, 1)
-    public Args_given___correct_difference(a: number, b: number, res: number): void {
-        const sut = new Math();
+	@TestCase(0, 0, 0)
+	@TestCase(1, 2, -1)
+	@TestCase(2, 1, 1)
+	public Args_given___correct_difference(a: number, b: number, res: number): void {
+		const sut = new Math();
 
-        const actual = sut.Subtract(a, b);
+		const actual = sut.Subtract(a, b);
 
-        Expect(actual).toBe(res);
-    }
+		Expect(actual).toBe(res);
+	}
 }
