13a6b9b05df06c838834ebb5dad311ae316e20d9
Günther Foidl <gue@korporal.at>
2020-05-02 13:43:58 +0200

    Updated demo

--- demo/app.ts
+++ demo/app.ts
@@ -3,5 +3,5 @@ import { Math } from "@gfoidl-tests/typescript-ci-test";
 const math = new Math();
 const a    = 3;
 const b    = 4;
-const sum  = math.Add(a, b);
+const sum  = math.add(a, b);
 console.log(`${a} + ${b} = ${sum}`);
