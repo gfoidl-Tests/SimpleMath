3f3ed15c221899301d7317e073ae8593a6de0077
Günther Foidl <gue@korporal.at>
2020-05-16 19:14:50 +0200

    Demo for local feed (#9)
    * Demo for local feed
    
    * Explicit name for publish artifact
    
    Fails otherwise. Maybe the wildcard is wrong...

---
+++ demo/local-feed/app.ts
@@ -0,0 +1,7 @@
+import { Math } from "@gfoidl-tests/typescript-ci-test";
+//-----------------------------------------------------------------------------
+const math = new Math();
+const a    = 3;
+const b    = 4;
+const sum  = math.add(a, b);
+console.log(`${a} + ${b} = ${sum}`);
